5011236655b3c89644d8a83fce48a8e437c1f1dd
syedhsan <ahsan.afzal@whiteboxtech.net>
2023-09-22 18:06:06 +0500

    Beautiful Dice Flutter Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
+  return runApp(MaterialApp(
+    home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dice App')),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
-      ),
-    ),
-  );
+        body: Dicepage()),
+  ));
 }
 
-class DicePage extends StatelessWidget {
+class Dicepage extends StatefulWidget {
+  const Dicepage({super.key});
+
+  @override
+  State<Dicepage> createState() => _DicepageState();
+}
+
+class _DicepageState extends State<Dicepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice1.png'))),
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice2.png')))
+        ],
+      ),
+    );
   }
 }
